3cfee176728382df67cc820f298bd1753de983e4
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-01 19:23:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
+OWNER
+
+require 'pry'
+
 class Owner
-  # code goes here
+
+  @@all_owners = Array.new
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all_owners.push(self)
+
+    @pets = Hash.new
+    @pets[:cats] = Array.new
+    @pets[:dogs] = Array.new
+    @pets[:fishes] = Array.new
+
+  end
+
+  def species
+    @species = "human"
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def pets
+    @pets
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes].push(new_fish)
+    #binding.pry
+  end
+
+
+
+
+
+
+
+
+#--------------------Class Methods------------------------------
+  def self.all
+    @@all_owners
+  end
+
+  def self.count
+    @@all_owners.count
+  end
+
+  def self.reset_all
+    @@all_owners.clear
+  end
+
+  def self.new=(name)
+    new_owner = Owner.new(name)
+  end
+
 end
